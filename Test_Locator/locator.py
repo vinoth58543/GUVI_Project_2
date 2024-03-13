623e9179680a7ebc696cd82ef6cd9d06eaa28aa9
vinoth58543 <145655177+vinoth58543@users.noreply.github.com>
2024-03-13 15:41:35 +0530

    Add files via upload

---
+++ Test_Locator/locator.py
@@ -0,0 +1,12 @@
+class Locators:
+    #locator for forget password
+    forgot_your_password = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[4]/p'
+    username_input_box = 'username'
+    reset_button = '//*[@id="app"]/div[1]/div[1]/div/form/div[2]/button[2]'
+
+
+    #locator for title
+    password_input_box = "password"
+    submit_button = '//html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
+    admin = '//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[1]/a'
+
