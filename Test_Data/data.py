6e229eb1475fb65edd856de9fbe32f59030c0902
vinoth58543 <145655177+vinoth58543@users.noreply.github.com>
2024-03-13 15:41:06 +0530

    Add files via upload

---
+++ Test_Data/data.py
@@ -0,0 +1,11 @@
+class Data:
+    #data for forgot password
+    url = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
+    username = 'Admin'
+
+    #data for titles
+
+    dashboard_url = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
+    password = "admin123"
+
+    
